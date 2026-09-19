3753057d235f21e8e861481d7a93d10ce87358ab
najoan125 <tiaaa0012@gmail.com>
2026-09-19 17:50:25 +0900

    Prevent iOS auto-zoom on form inputs

--- inc/functions.php
+++ inc/functions.php
@@ -7,7 +7,7 @@ function baseUrl(): string {
 }
 
 function assetUrl(string $path): string {
-    return '/anime/assets/' . ltrim($path, '/') . '?v=144';
+    return '/anime/assets/' . ltrim($path, '/') . '?v=145';
 }
 
 function coverUrl(string $filename): string {
