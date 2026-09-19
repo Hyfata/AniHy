c0dc451b23d0004a1e9d29bd60745c95104909ad
najoan125 <tiaaa0012@gmail.com>
2026-09-19 17:48:27 +0900

    Open queue modal above anime modal

--- inc/functions.php
+++ inc/functions.php
@@ -7,7 +7,7 @@ function baseUrl(): string {
 }
 
 function assetUrl(string $path): string {
-    return '/anime/assets/' . ltrim($path, '/') . '?v=143';
+    return '/anime/assets/' . ltrim($path, '/') . '?v=144';
 }
 
 function coverUrl(string $filename): string {
