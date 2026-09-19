6d1ede46416a56b499674a2935c93d558d4848b3
najoan125 <tiaaa0012@gmail.com>
2026-09-19 10:33:29 +0900

    Prevent mobile text inflation on rotation

--- inc/functions.php
+++ inc/functions.php
@@ -7,7 +7,7 @@ function baseUrl(): string {
 }
 
 function assetUrl(string $path): string {
-    return '/anime/assets/' . ltrim($path, '/') . '?v=141';
+    return '/anime/assets/' . ltrim($path, '/') . '?v=142';
 }
 
 function coverUrl(string $filename): string {
