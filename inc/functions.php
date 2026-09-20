72896fc79771d6f5ef7061ec1998af26b8a565b7
najoan125 <tiaaa0012@gmail.com>
2026-09-21 02:43:31 +0900

    Fix: anti-aliasing not working correctly on anime card

--- inc/functions.php
+++ inc/functions.php
@@ -7,7 +7,7 @@ function baseUrl(): string {
 }
 
 function assetUrl(string $path): string {
-    return '/anime/assets/' . ltrim($path, '/') . '?v=167';
+    return '/anime/assets/' . ltrim($path, '/') . '?v=168';
 }
 
 function coverUrl(string $filename): string {
